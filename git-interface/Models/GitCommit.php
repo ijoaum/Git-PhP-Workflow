1f738f0eabce74a5889047749585b2dd785ac898
João Victor <ijoaum@gmail.com>
2012-11-15 17:49:48 -0200

    commit do php '

--- git-interface/Models/GitCommit.php
+++ git-interface/Models/GitCommit.php
@@ -9,14 +9,14 @@ class GitCommit extends Git {
 	public $sha1;
 	public $author;
 	public $date;
-	public $mesage;
+	public $message;
 	
 	function __construct($commit) {
 		$this->id 		= substr($commit->commit, 0, 6);
 		$this->sha1		= $commit->commit;
 		$this->author 	= $commit->author;
 		$this->date 	= $commit->date;
-		$this->mesage	= $commit->mesage;
+		$this->message	= $commit->message;
 	}
 	
 	public static function doCommit($message){
